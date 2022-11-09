b6d80355b310d6b58550e073c383d89679088443
David Sternlicht <d1618033@gmail.com>
2022-11-09 10:19:15 +0200

    Add test for nested task

--- tests/test_celery_flask_login.py
+++ tests/test_celery_flask_login.py
@@ -4,6 +4,8 @@ import celery_flask_login
 from flask_login import LoginManager, UserMixin, login_user
 from dataclasses import dataclass
 from flask import Flask, jsonify
+from celery.result import AsyncResult
+import time
 
 
 @dataclass
@@ -37,7 +39,12 @@ def test_basic_task(celery_app, celery_worker, flask_app, flask_client, login_ma
 
     @celery_app.task
     def echo(x, y, z):
-        return {"x": x, "y": y, "z": z, "user_id": current_user.id if current_user else None}
+        return {
+            "x": x,
+            "y": y,
+            "z": z,
+            "user_id": current_user.id if current_user else None,
+        }
 
     celery_worker.reload()
 
@@ -51,4 +58,52 @@ def test_basic_task(celery_app, celery_worker, flask_app, flask_client, login_ma
         "y": 2,
         "z": 5,
         "user_id": "dave",
+    }
+
+
+@pytest.fixture(scope="session")
+def celery_worker_pool():
+    return "threads"
+
+
+def test_nested_task(celery_app, celery_worker, flask_app, flask_client, login_manager):
+    celery_flask_login.setup(flask_app)
+
+    @celery_app.task
+    def outer(x):
+        return {"inner_task_id": inner.delay(x).id}
+
+    @celery_app.task
+    def inner(x):
+        return {"x": x, "user_id": current_user.id if current_user else None}
+
+    celery_worker.reload()
+
+    @flask_app.route("/")
+    def index():
+        login_user(User(id="dave"))
+        return jsonify({"task_id": outer.delay(1).id})
+
+    @flask_app.route("/tasks/<task_id>")
+    def task(task_id):
+        task = AsyncResult(task_id)
+        if task.status != "PENDING":
+            result = task.get()
+        else:
+            result = None
+        return jsonify({"status": task.status, "result": result})
+
+    task_id = flask_client.get("/").json["task_id"]
+    while (
+        task_status := flask_client.get(f"/tasks/{task_id}").json["status"]
+    ) == "PENDING":
+        time.sleep(1)
+    task_id = flask_client.get(f"/tasks/{task_id}").json["result"]["inner_task_id"]
+    while (
+        task_status := flask_client.get(f"/tasks/{task_id}").json["status"]
+    ) == "PENDING":
+        time.sleep(1)
+    assert flask_client.get(f"/tasks/{task_id}").json == {
+        "status": "SUCCESS",
+        "result": {"x": 1, "user_id": "dave"},
     }
